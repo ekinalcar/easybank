12f46fbf7503b53ee7b794fae780b851ce9fb779
Ekin Alçar <ekinalcar@gmail.com>
2021-01-04 19:47:52 +0100

    added menu open and close

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
-console.log('1');
+const btnHamburger = document.querySelector('#btnHamburger');
+btnHamburger.addEventListener('click', () => {
+  if (btnHamburger.classList.contains('open')) {
+    return btnHamburger.classList.remove('open');
+  }
+  return btnHamburger.classList.add('open');
+});
